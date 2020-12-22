08d23363febf1c8ff24660bebd69f9d4940446ac
MD. Muhtasim Fuad <48941230+muhtasim50@users.noreply.github.com>
2020-12-22 21:32:26 +0600

    Add files via upload

---
+++ Library Management/Shoot/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Library Management System 2020| Designed by : Yo
+                </div>
+
+            </div>
+        </div>
+    </section>
